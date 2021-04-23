3630478bb59984305c53857507cf180365ba8ab4
TacticalTechJay <gogojayvin923@gmail.com>
2021-04-22 21:48:43 -0700

    Removed Sentry

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const Sentry = require('@sentry/node');
 const op = require('./config.json');
 const DBLWorker = require('./DBLWorker');
 
-if (op['Sentry-DSN']) Sentry.init({ dsn: op['Sentry-DSN'] });
 new DBLWorker(op);
